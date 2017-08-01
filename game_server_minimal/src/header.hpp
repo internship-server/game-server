d3f457a135346d2cbc27a7e505fd92c22fa31f70
WyldeCat <skan1543@gmail.com>
2017-08-01 14:37:14 +0900

    Implement test purpose server

---
+++ game_server_minimal/src/header.hpp
@@ -0,0 +1,14 @@
+//
+// header.hpp
+//
+
+#pragma once
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+
+#define CLIENT_PORT 4001
+#define CLIENT_IP "127.0.0.1"
+
+#pragma comment(lib, "ws2_32.lib")
--- game_server_minimal/src/main.cpp
+++ game_server_minimal/src/main.cpp
@@ -1,4 +1,53 @@
+//
+// main.cpp
+//
+
+#include "header.hpp"
+
+#include <iostream>
+
+struct test_packet {
+    int x;
+    //int y;
+};
+
 int main()
 {
+    // init wsa
+    WSADATA wsa_data;
+    int ret = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (ret != 0) return 1;
+
+    SOCKET client_socket = ::socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    std::cout << client_socket << std::endl;
+
+    SOCKADDR_IN client_addr;
+
+    memset(&client_addr, 0, sizeof(client_addr));
+
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(CLIENT_PORT);
+    InetPtonA(AF_INET, "127.0.0.1", &client_addr.sin_addr);
+
+    test_packet sample_packet;
+    
+    sample_packet.x = 1000;
+    //sample_packet.y = 600;
+
+    while (1) {
+        std::cout << "Send...\n";
+        int ret = sendto(client_socket,
+            reinterpret_cast<const char*>(&sample_packet),
+            sizeof(sample_packet), 0,
+            reinterpret_cast<const sockaddr*>(&client_addr),
+            sizeof(client_addr));
+        
+        if (ret == SOCKET_ERROR) break;
+        std::cout << "Sent! " << ret << "\n";
+        Sleep(1000);
+    }
+
+    closesocket(client_socket);
+    WSACleanup();
     return 0;
 }
